aeebc990d0fb8186c4ea0396c654a26a9e898e22
krmax44 <hi@krmax44.de>
2019-05-24 18:29:57 +0200

    chore: code style

--- theme/utils/element-position.js
+++ theme/utils/element-position.js
@@ -1,7 +1,7 @@
 export default function elementPosition(el) {
 	const computedStyle = window.getComputedStyle(el);
 	const boundingRect = el.getBoundingClientRect();
-	const {width, height} = boundingRect;
+	const { width, height } = boundingRect;
 	const top =
 		el.offsetTop - parseInt(computedStyle.getPropertyValue('margin-top'), 10);
 
@@ -9,5 +9,5 @@ export default function elementPosition(el) {
 		boundingRect.left -
 		parseInt(computedStyle.getPropertyValue('margin-left'), 10);
 
-	return {top, left, width, height};
+	return { top, left, width, height };
 }
