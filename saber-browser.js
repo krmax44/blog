acd158a281410543eccfbd604788da5108e42445
krmax44 <hi@krmax44.de>
2019-11-25 17:10:51 +0100

    chore: formatting

--- saber-browser.js
+++ saber-browser.js
@@ -5,5 +5,5 @@ import './theme/assets/scss/global.scss';
 import BadgeButton from './theme/components/BadgeButton.vue';
 
 export default ({ Vue }) => {
-  Vue.component('BadgeButton', BadgeButton);
+	Vue.component('BadgeButton', BadgeButton);
 };
